22438a8ac50ba882ecb4c5474f58c6ac03742269
MRJ <>
2019-01-12 23:30:19 +0800

    create sql file!

---
+++ sql.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var router = express.Router();
+var mysql = require('mysql');
+
+app.use(router);
+router.get('/allcaipaio', function(req, res){
+    var con = mysql.createConnection({
+        'host':'localhost',
+        'user':'root',
+        'password':'897011805',
+        'database':'yhj'
+    })
+
+    con.connect()
+    con.query('SELECT *FROM caipiao', function(err, results){
+        if err throw err;
+        var datas = [];
+        for (va in results) {
+            var value = {"period": va.period}
+            datas.push(value);
+        }
+        res.send(JSON.stringify(datas))
+    })
+})
+
+app.listen(3000, function(err){
+    if err throw err;
+    console.log('server starting!!')
+})
